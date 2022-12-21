0306961a93bcbfed7e33854365fb9a1b951e1b43
Mikhail Ovsyannikov (rmlib.null) <rmlib.null@gmail.com>
2022-12-21 16:38:56 +0300

    start

---
+++ examples.v
@@ -0,0 +1,39 @@
+// v
+//import os
+import math {abs, round_sig, fmod}
+
+fn hello() string {
+	return 'Hello, World!'
+}
+
+fn celsius_to_farenheit(degree f64) f64 {
+	return round_sig((degree * 9.0 / 5.0) + 32.0, 1)
+}
+
+fn farenheit_to_celsius(degree f64) f64 {
+	return round_sig((degree - 32.0) * 5.0 / 9.0, 1)
+}
+
+fn is_even(number f64) bool {
+	if fmod(number, 2) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+struct Player {
+	name	string
+	age		int
+	class	string
+}
+
+fn main() {
+	player := Player {
+		name: "Я"
+	}
+
+	if player.name == "Я" {
+		println("Я")
+	}
+}
